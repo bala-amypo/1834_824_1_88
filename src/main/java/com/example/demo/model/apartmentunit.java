6425fe2a2197d6a31202555f97f219c0cdf9800c
bala-amypo <bala@amypo.in>
2025-12-17 08:48:32 +0000

    User commit

--- src/main/java/com/example/demo/model/apartmentunit.java
+++ src/main/java/com/example/demo/model/apartmentunit.java
@@ -1,1 +1,31 @@
-package com.
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.OneToOne;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApartmentUnit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String unitNumber;
+
+    private Integer floor;
+
+    @OneToOne
+    private User owner;
+}
