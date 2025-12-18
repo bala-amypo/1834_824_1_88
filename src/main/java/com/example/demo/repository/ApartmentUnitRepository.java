9ecc24a2a610c277aa64e1f653d1025953d7529f
bala-amypo <bala@amypo.in>
2025-12-18 07:49:32 +0000

    User commit

--- src/main/java/com/example/demo/repository/ApartmentUnitRepository.java
+++ src/main/java/com/example/demo/repository/ApartmentUnitRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.ApartmentUnit;
 
 public interface ApartmentUnitRepository extends JpaRepository<ApartmentUnit, Long> {
-} 
+    
+
+}         
