a773885d6de3003532567761bb3d7a958e3068bb
bala-amypo <bala@amypo.in>
2025-12-23 09:22:15 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.model.User;
 import com.example.demo.service.UserService;
 
-@RestController
+@RestController 
 @RequestMapping("/users")
 public class UserController {
 
