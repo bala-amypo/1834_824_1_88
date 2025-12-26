76737b2598d9f5f24db09fdadd69456b361d65a2
bala-amypo <bala@amypo.in>
2025-12-26 04:58:18 +0000

    User commit

--- src/main/java/com/example/demo/model/Facility.java
+++ src/main/java/com/example/demo/model/Facility.java
@@ -6,7 +6,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Column;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Facility {
 
     @Id
@@ -18,50 +25,9 @@ public class Facility {
 
     private String description;
 
+    // HH:mm
     private String openTime;
 
+    // HH:mm
     private String closeTime;
-
-    public Facility() {
-    }
-
-    public Long getId() {
-        return id;
-    }
- 
-    public void setId(Long id) {
-        this.id = id;
-    }
- 
-    public String getName() {
-        return name;
-    }
- 
-    public void setName(String name) {
-        this.name = name;
-    }
- 
-    public String getDescription() {
-        return description;
-    }
- 
-    public void setDescription(String description) {
-        this.description = description;
-    }
- 
-    public String getOpenTime() {
-        return openTime;
-    }
- 
-    public void setOpenTime(String openTime) {
-        this.openTime = openTime;
-    }
- 
-    public String getCloseTime() {
-        return closeTime;
-    }
- 
-    public void setCloseTime(String closeTime) {
-        this.closeTime = closeTime;
-    }
 }
--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -7,8 +7,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Column;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
 @Entity
 @Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
@@ -22,48 +29,6 @@ public class User {
 
     private String password;
 
+    // RESIDENT / ADMIN
     private String role;
-
-    public User() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
- 
-    public void setPassword(String password) {
-        this.password = password;
-    }
- 
-    public String getRole() {
-        return role;
-    }
- 
-    public void setRole(String role) {
-        this.role = role;
-    }
 }
