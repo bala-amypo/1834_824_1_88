28204ab766b078d20ad6c599cfbb40eea785dcc1
bala-amypo <bala@amypo.in>
2025-12-27 05:32:54 +0000

    User commit

--- src/main/java/com/example/demo/controller/FacilityController.java
+++ src/main/java/com/example/demo/controller/FacilityController.java
@@ -1,3 +1,32 @@
+// package com.example.demo.controller;
+
+// import com.example.demo.model.Facility;
+// import com.example.demo.service.FacilityService;
+// import org.springframework.web.bind.annotation.*;
+
+// import java.util.List;
+
+// @RestController
+// @RequestMapping("/facilities")
+// public class FacilityController {
+
+//     private final FacilityService service;
+
+//     public FacilityController(FacilityService service) {
+//         this.service = service;
+//     }
+
+//     @PostMapping
+//     public Facility addFacility(@RequestBody Facility facility) {
+//         return service.addFacility(facility);
+//     }
+
+//     @GetMapping
+//     public List<Facility> getAllFacilities() {
+//         return service.getAllFacilities();
+//     }
+// }
+
 package com.example.demo.controller;
 
 import com.example.demo.model.Facility;
@@ -16,11 +45,13 @@ public class FacilityController {
         this.service = service;
     }
 
+    // ------------------ Add Facility ------------------
     @PostMapping
     public Facility addFacility(@RequestBody Facility facility) {
         return service.addFacility(facility);
     }
 
+    // ------------------ Get All Facilities ------------------
     @GetMapping
     public List<Facility> getAllFacilities() {
         return service.getAllFacilities();
