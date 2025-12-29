cccfc75f6cc987ddcefd9b1df684f9dee847fa73
bala-amypo <bala@amypo.in>
2025-12-29 14:39:31 +0000

    User commit

--- src/main/java/com/example/demo/dto/RegisterRequest.java
+++ src/main/java/com/example/demo/dto/RegisterRequest.java
@@ -1,19 +1,52 @@
 package com.example.demo.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public class RegisterRequest {
 
+    @Schema(example = "Sharan")
     private String name;
+
+    @Schema(example = "sharan2007@gmail.com")
     private String email;
+
+    @Schema(example = "sharan96")
     private String password;
+
+    @Schema(example = "ADMIN")
+    private String role;
 
     public RegisterRequest() {}
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public String getName() {
+        return name;
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+ 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+ 
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
